cfa8f21f215d163117b84e69134cfb0f73b6b56a
Mike Cheung <zhtmike@gmail.com>
2025-06-17 14:25:22 +0800

    better test scripts

--- tool/offline_inference_blip2.py
+++ tool/offline_inference_blip2.py
@@ -1,17 +1,19 @@
-import vllm_mindspore # Add this line on the top of script.
+import vllm_mindspore  # Add this line on the top of script.
 from PIL import Image
 from vllm import LLM, SamplingParams
+
+
+def prepare_text(prompt: str):
+    text = f"Question: {prompt} Answer:"
+    return text
 
 
 def main(args):
     model_path = args.model_path
 
-    text = "Question: Describe this image. Answer:"
+    prompts = ["Describe this photo.", "Guess where and when this photo was taken."]
 
-    print("========== input =========")
-    print(text)
-    print("==========================")
-
+    texts = [prepare_text(prompt) for prompt in prompts]
 
     # Load image using PIL.Image
     image = Image.open(args.image_path)
@@ -20,30 +22,34 @@ def main(args):
     sampling_params = SamplingParams(temperature=0.0, top_p=0.95, max_tokens=512)
 
     # Create an LLM.
-    llm = LLM(model=model_path,
-            max_num_seqs=8,
-            tensor_parallel_size=args.tp_size,
-            )
+    llm = LLM(
+        model=model_path,
+        max_num_seqs=8,
+        tensor_parallel_size=args.tp_size,
+    )
 
     # Generate texts from the prompts. The output is a list of RequestOutput objects
     # that contain the prompt, generated text, and other information.
-    query = {"prompt": text, "multi_modal_data": {"image": image}}
-    inputs = [query for i in range(args.batch_size)] 
+    inputs = [
+        {"prompt": texts[i], "multi_modal_data": {"image": image}}
+        for i in range(len(texts))
+    ]
     outputs = llm.generate(inputs, sampling_params)
 
     # Print the outputs.
-    print("========== output =========")
-    for output in outputs:
+    for prompt, output in zip(prompts, outputs):
         generated_text = output.outputs[0].text
-        print(generated_text)
-    print("==========================")
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser(description="test")
-    parser.add_argument("--model_path", type=str, default="/home/mikecheung/model/blip2-opt-2.7b")
+    parser.add_argument(
+        "--model_path", type=str, default="/home/mikecheung/model/blip2-opt-2.7b"
+    )
     parser.add_argument("--image_path", type=str, default="demo.jpeg")
-    parser.add_argument("--batch_size", type=int, default=2)
     parser.add_argument("--tp_size", type=int, default=1)
     args, _ = parser.parse_known_args()
 
